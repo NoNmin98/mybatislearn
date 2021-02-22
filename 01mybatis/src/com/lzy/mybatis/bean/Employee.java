f056a5124f3b58ea341ceee8ebc8512de26db30c
NoNmin98 <32301978+NoNmin98@users.noreply.github.com>
2021-02-22 08:55:22 +0800

    2021.2.22

--- 01mybatis/src/com/lzy/mybatis/bean/Employee.java
+++ 01mybatis/src/com/lzy/mybatis/bean/Employee.java
@@ -1,15 +1,30 @@
 package com.lzy.mybatis.bean;
 
+import org.apache.ibatis.type.Alias;
+
 /**
  * @author: lzy
  * @description: 创建员工类
  * @date: 2021-02-18-14:32
  */
+//这里的别名可以简化xml文件中的resultType
 public class Employee {
     private Integer id;
     private String lastName;
     private String email;
     private String gender;
+    private Department dept;
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String lastName, String email, String gender, Department dept) {
+        this.id = id;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.dept = dept;
+    }
 
     @Override
     public String toString() {
@@ -18,7 +33,16 @@ public class Employee {
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
                 ", gender='" + gender + '\'' +
+                ", dept=" + dept +
                 '}';
+    }
+
+    public Department getDept() {
+        return dept;
+    }
+
+    public void setDept(Department dept) {
+        this.dept = dept;
     }
 
     public Integer getId() {
--- 01mybatis/src/com/lzy/mybatis/test/MybatisTest.java
+++ 01mybatis/src/com/lzy/mybatis/test/MybatisTest.java
@@ -1,6 +1,7 @@
 package com.lzy.mybatis.test;
 
 import com.lzy.mybatis.bean.Employee;
+import com.lzy.mybatis.dao.EmployeeMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -34,6 +35,72 @@ public class MybatisTest {
         Employee employee = openSession.selectOne("com.lzy.mybatis.EmployeeMapper.selectEmp", 1);
         System.out.println(employee.toString());
         //关闭session,推荐使用try catch
+        openSession.close();
+    }
+
+    @Test
+    public void test1() throws IOException {
+        //1.获取sqlSessionFactory对象
+        String resource = "mybatis-conf.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+        //2.获取sqlSession对象
+        SqlSession openSession = sqlSessionFactory.openSession();
+        //3.获取接口的实现类对象
+        EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
+        Employee empById = mapper.getEmpById(1);
+        System.out.println(empById);
+        //4.关闭资源
+        openSession.close();
+    }
+    //测试增删改
+//    @Test
+//    public void test2() throws IOException {
+//        //1.获取sqlSessionFactory对象
+//        String resource = "mybatis-conf.xml";
+//        InputStream inputStream = Resources.getResourceAsStream(resource);
+//        SqlSessionFactory sqlSessionFactory =
+//                new SqlSessionFactoryBuilder().build(inputStream);
+//        //2.获取sqlSession对象
+//        //使用这个openSession不会自动提交我们需要手动提交
+//        SqlSession openSession = sqlSessionFactory.openSession();
+//
+//        try {
+//            EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
+//            Employee employee = new Employee(null, "lue", "897@qq.com", "1");
+//            mapper.addEmp(employee);
+//            //需要自动提交
+//            openSession.commit();
+//        }finally{
+//            openSession.close();
+//        }
+//    }
+
+    @Test
+    public void test3() throws IOException {
+        String resource = "mybatis-conf.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession openSession = sqlSessionFactory.openSession();
+        EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
+        Employee empById = mapper.getEmpById(1);
+        System.out.println(empById);
+        openSession.close();
+    }
+
+    @Test
+    public void test4() throws IOException {
+        String resource = "mybatis-conf.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession openSession = sqlSessionFactory.openSession();
+        EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
+        Employee empById = mapper.getEmpAndDept(1);
+        System.out.println(empById);
+        System.out.println(empById.getDept());
         openSession.close();
     }
 }
